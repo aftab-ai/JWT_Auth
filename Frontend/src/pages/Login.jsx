3cab971ba7e93fbe4d6c7907301a431c592c884b
Aftab Jariwala <aftabjariwala16@gmail.com>
2026-02-04 15:21:26 +0530

    Signup and Login page design.

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,10 +1,59 @@
+// Third-Party modules.
 import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <div>
-      <p>Login</p>
-      <Link to="/forgot-password">Forgot Password</Link>
+    // Login Form
+    <div className="h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center h-3/5 w-xl p-8 bg-[#D3D2C7] rounded-lg">
+        {/* Heading */}
+        <h2 className="text-[#10403B] font-bold text-2xl">Login</h2>
+
+        <form className="text-[#10403B] font-semibold flex flex-col gap-4 w-full">
+          {/* Email */}
+          <div className="flex flex-col">
+            <label htmlFor="email" className="text-lg">
+              Email:
+            </label>
+            <input
+              className="p-1 rounded-lg border-[#148B4B] border-2"
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Enter email..."
+            />
+          </div>
+
+          {/* Password */}
+          <div className="flex flex-col">
+            <label htmlFor="password" className="text-lg">
+              Password:
+            </label>
+            <input
+              className="p-1 rounded-lg border-[#148B4B] border-2"
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter password..."
+            />
+          </div>
+
+          {/* Forgot-Password */}
+          <Link to="/forgot-password" className="underline font-bold text-sm">
+            Forgot Password...
+          </Link>
+
+          {/* Login Button */}
+          <div className="text-center">
+            <input
+              className="rounded-full px-4 py-1 mt-4 cursor-pointer bg-[#10403B]
+               text-white hover:bg-[#4C5958]"
+              type="submit"
+              value={"Login"}
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,5 +1,81 @@
 function Signup() {
-  return <div>Signup</div>;
+  return (
+    // Signup Form
+    <div className="h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center h-4/5 w-xl p-8 bg-[#D3D2C7] rounded-lg">
+        {/* Heading */}
+        <h2 className="text-[#10403B] font-bold text-2xl">Sign Up</h2>
+
+        <form className="text-[#10403B] font-semibold flex flex-col gap-4 w-full">
+          {/* Username */}
+          <div className="flex flex-col">
+            <label htmlFor="username" className="text-lg">
+              Username:
+            </label>
+            <input
+              className="p-1 rounded-lg border-[#148B4B] border-2"
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Enter name..."
+            />
+          </div>
+
+          {/* Email */}
+          <div className="flex flex-col">
+            <label htmlFor="email" className="text-lg">
+              Email:
+            </label>
+            <input
+              className="p-1 rounded-lg border-[#148B4B] border-2"
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Enter email..."
+            />
+          </div>
+
+          {/* Password */}
+          <div className="flex flex-col">
+            <label htmlFor="password" className="text-lg">
+              Password:
+            </label>
+            <input
+              className="p-1 rounded-lg border-[#148B4B] border-2"
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter password..."
+            />
+          </div>
+
+          {/* Confirm Password */}
+          <div className="flex flex-col">
+            <label htmlFor="confirmPassword" className="text-lg">
+              Confirm Password:
+            </label>
+            <input
+              className="p-1 rounded-lg border-[#148B4B] border-2"
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="Enter password again..."
+            />
+          </div>
+
+          {/* Signup Button */}
+          <div className="text-center">
+            <input
+              className="rounded-full px-4 py-1 mt-4 cursor-pointer bg-[#10403B]
+               text-white hover:bg-[#4C5958]"
+              type="submit"
+              value={"Signup"}
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
