812ff2b54db796a7be83a26ec1cfdd8e03c3703b
Aftab Jariwala <aftabjariwala16@gmail.com>
2026-03-24 11:40:26 +0530

    Logout-All route docs.

--- Frontend/src/components/docsComponents/LogoutAllRoute.jsx
+++ Frontend/src/components/docsComponents/LogoutAllRoute.jsx
@@ -1,5 +1,80 @@
 function LogoutAllRoute() {
-  return <div>LogoutAllRoute</div>;
+  return (
+    <div className="m-4 text-justify">
+      {/* Title */}
+      <h1 className="font-bold text-2xl text-[#10403B]">
+        Logout-Sessions Route
+      </h1>
+
+      {/* List */}
+      <div className="mt-2 p-2 text-sm border border-[#B7BDA9] rounded">
+        <ul className="list-disc list-outside ml-8">
+          <li>
+            Terminates all the sessions and logs the user out from the server.
+          </li>
+          <li>
+            All Access-Token, CSRF-Token and Refresh-Token would be removed from
+            the servers.
+          </li>
+          <li>The cookie would be cleared from the client.</li>
+          <li>Logout requires both a Refresh-Token and a CSRF-Token.</li>
+        </ul>
+      </div>
+
+      {/* Example Request */}
+      <div className="mt-4">
+        <h2 className="font-medium text-lg text-[#4C5958]">Example Request:</h2>
+        <div className="mt-1 p-1 border border-[#B7BDA9] rounded">
+          <code className="block leading-relaxed py-1 pl-2 rounded text-sm bg-[#B3C1A8]">
+            Endpoint:
+            <span className="text-[#127369]"> POST</span>{" "}
+            "/api/v1/auth/logout-all"
+          </code>
+          <pre className="max-w-full px-2 py-1 font-mono rounded text-xs sm:text-sm bg-[#BFBFBF] overflow-x-auto whitespace-pre scrollbar-thin">
+            <code className="block leading-relaxed">
+              {`Cookie: refreshToken=Refresh_Token
+x-csrf-token: CSRF_Token`}
+            </code>
+          </pre>
+        </div>
+      </div>
+
+      {/* Example Response */}
+      <div className="mt-4">
+        <h2 className="font-medium text-lg text-[#4C5958]">
+          Example Response:
+        </h2>
+        <div className="mt-1 p-1 border border-[#B7BDA9] rounded">
+          <pre className="max-w-full px-2 py-1 font-mono rounded text-xs sm:text-sm bg-[#BFBFBF] overflow-x-auto whitespace-pre scrollbar-thin">
+            <code className="block leading-relaxed">
+              {`{
+  "statusCode": "200",
+  "status": "true",
+  "code": "SESSIONS_DELETED",
+  "message": "User logged out successfully from all the sessions.",
+  "data": {
+    "csrfToken": null
+  }
+}`}
+            </code>
+          </pre>
+
+          {/* Cookie */}
+          <code className="block leading-relaxed mt-4 py-1 pl-2 rounded text-sm bg-[#B3C1A8]">
+            Cookie:
+          </code>
+          <pre className="max-w-full px-2 py-1 font-mono rounded text-xs sm:text-sm bg-[#BFBFBF] overflow-x-auto whitespace-pre scrollbar-thin">
+            <code className="block leading-relaxed">
+              {`{
+  "accessToken": Clear
+  "refreshToken": Clear
+}`}
+            </code>
+          </pre>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LogoutAllRoute;
